a0dc5d1929b38a67dd54c88e5a563a65e621d2e5
jakebathman <jake.bathman@gmail.com>
2020-09-25 17:23:52 -0500

    Make `Project` an eloquent model and refactor
    - Add migration for new `projects` table
    - Convert `App\Project` to be an eloquent model for this table
    - Refactor the methods in the old `Project` to work with the model attributes and assume data exists in the table
    - Add new command `stats:fetch` to actually pull project data and store it in the `projects` table
    - Add schedule for `stats:fetch` to run hourly
    - Rename `Pr` and `Prs` in many places to be `PullRequest` and `PullRequests`
    - Delete the now-useless `App\Projects`
    - Add packages `novapackages` and `takeout` to `projects.json`
    - Add relative update time to subhead of each package table in `components.project` view

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('stats:fetch')
+            ->hourly();
+
         $schedule->command('stats:slack')
             ->weekly()
             ->fridays()
--- resources/views/components/project.blade.php
+++ resources/views/components/project.blade.php
@@ -1,12 +1,15 @@
 <div class="mt-16">
     <section class="flex justify-between items-center py-6 border-b-2 border-clouds"
              id="project-{{ $project->namespace }}-{{ $project->name }}">
-        <h2 class="w-1/2 text-2xl text-black font-semibold tracking-wide">
-            <a href="{{ $project->url() }}" aria-label="Launch">
-                {{ $project->namespace }} | {{ $project->name }}
-                @include('svg.launch')
-            </a>
-        </h2>
+        <div class="w-1/2">
+            <h2 class="text-2xl text-black font-semibold tracking-wide">
+                <a href="{{ $project->url() }}" aria-label="Launch">
+                    {{ $project->namespace }} | {{ $project->name }}
+                    @include('svg.launch')
+                </a>
+            </h2>
+            <div class="text-grey-dark font-thin">{{ $project->updatedAt() }}</div>
+        </div>
 
         <p class="w-1/2 text-right text-black-lightest">
             Maintained by
@@ -23,7 +26,7 @@
         <h3 class="w-1/3 text-lg text-black-lightest py-6 font-thin">Pull Requests</h3>
 
         <ul class="w-2/3">
-            @foreach ($project->prs() as $pr)
+            @foreach ($project->pull_requests as $pr)
                 <x-pull-request :pr-data="$pr" :project="$project" />
             @endforeach
         </ul>
@@ -33,7 +36,7 @@
         <h3 class="w-1/3 text-lg text-black-lightest py-6 font-thin">Issues</h3>
 
         <ul class="w-2/3 list-reset">
-            @foreach ($project->issues() as $issue)
+            @foreach ($project->issues as $issue)
                 <x-issue :issue="$issue" :project="$project" />
             @endforeach
         </ul>
